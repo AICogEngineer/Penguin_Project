e14cec10ab985b0e1484620153df711a77e02d20
LawsonLay <lawsonlay04@gmail.com>
2026-01-20 14:10:42 -0600

    feat: single agent WIP

--- legacy/gradio_intogration.py
+++ legacy/gradio_intogration.py
@@ -2,7 +2,7 @@ import gradio as gr
 import os
 import boto3
 import uuid
-from langchain_aws import ChatBedrockConverse
+from langchain_aws import ChatBedrock
 from langchain.messages import AIMessage, HumanMessage 
 from langchain.agents import create_agent
 from langchain_core.tools import tool
@@ -11,39 +11,50 @@ from dotenv import load_dotenv
 
 # 1. Load the variables from .env
 load_dotenv()
+region=os.getenv("BEDROCK_AWS_REGION")
+llm = ChatBedrock(model_id="amazon.nova-lite-v1:0", region_name=region)
 
 @tool
 def calculate_sum(a: int, b: int) -> int:
     """Add two numbers together. Use for arithmetic operations."""
     return a + b
 
+@tool
+def user_credential_request() -> str:
+    """When the user request PII(Personally Identifiable Information) or financial transactions request credentials"""
+    return "Please provide your username,email and billing zip to contiue."
+
+@tool
+def verify_user_credential(username: str,email: str,zip: str) -> str:
+    """When the user provides his information ie his username,email and billing zip"""
+    return "request sent"
+
 agent = create_agent(
-    model='bedrock:us.amazon.nova-2-lite-v1:0',
+    model=llm,
     tools=[calculate_sum],
     checkpointer=InMemorySaver(),
     system_prompt="You are a helpful math assistant.",
     name="math_agent"  # Always provide a name!
+
 )
 
 THREAD_ID = str(uuid.uuid4())
 
 def predict(message, history):
-    history_langchain_format = []
-    for msg in history:
-        if msg['role'] == "user":
-            history_langchain_format.append(HumanMessage(content=msg['content']))
-        elif msg['role'] == "assistant":
-            history_langchain_format.append(AIMessage(content=msg['content']))
+    # 4. Config for Memory
     config = {"configurable": {"thread_id": THREAD_ID}}
-    history_langchain_format.append(HumanMessage(content=message))
+    
+    # 5. Invoke the Agent
     input_data = {"messages": [HumanMessage(content=message)]}
-    gpt_response = agent.invoke(input_data, config=config)
-    return gpt_response.content
+    
+    result = agent.invoke(input_data, config=config)
+    
+    # 6. Extract Response
+    return result['messages'][-1].content
 
 demo = gr.ChatInterface(
     predict,
-    api_name="chat",
-    placeholder="Hello! I am MathBot. Ask me to add numbers!"
+    api_name="chat"
 )
 
 demo.launch()
